4bdbfcc6685661de9d85b90c7ac830d74df59b0e
JoaoCarv30 <Carvalhoestudante1@gmail.com>
2025-04-15 00:43:16 -0300

    Text Button Parameter

--- src/components/button/button.component.ts
+++ src/components/button/button.component.ts
@@ -1,4 +1,4 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 
 @Component({
   selector: 'app-button',
@@ -8,4 +8,9 @@ import { Component } from '@angular/core';
 })
 export class ButtonComponent {
 
+
+  @Input() text : string = 'Button';
+
 }
+
+
