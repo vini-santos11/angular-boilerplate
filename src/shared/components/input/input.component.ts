5cc11ecc776c40b9dcc39908e922db56826e39dd
Marcus Vinicius <m.vinicius1122o@gmail.com>
2025-04-15 21:44:14 -0300

    feat: add show password to inputs types password

--- src/shared/components/input/input.component.ts
+++ src/shared/components/input/input.component.ts
@@ -2,7 +2,7 @@ import { CommonModule } from '@angular/common';
 import { Component, Input } from '@angular/core';
 import { MaskDirective } from '../../directives/mask.directive';
 import { ReactiveFormsModule } from '@angular/forms';
-import { LucideAngularModule } from 'lucide-angular';
+import { Eye, EyeOff, LucideAngularModule } from 'lucide-angular';
 
 @Component({
   selector: 'app-input',
@@ -13,13 +13,23 @@ import { LucideAngularModule } from 'lucide-angular';
 export class InputComponent {
   @Input() label: string = '';
   @Input() required: boolean = false;
-  @Input() type: string = 'text';
+  @Input() type: string = '';
   @Input() placeholder: string = '';
   @Input() mask: string = '';
   @Input() icon?: any;
   @Input() iconPosition: 'left' | 'right' = 'left' ;
 
+  public showPassword = false;
+  public initialType: string = '';
+
+  readonly Eye = Eye;
+  readonly EyeOff = EyeOff;
+
   constructor() {}
+
+  protected ngOnInit(): void {
+    this.initialType = this.type;
+  }
 
   get isIconLeft(): boolean {
     return !!this.icon && this.iconPosition === 'left';
@@ -27,5 +37,10 @@ export class InputComponent {
 
   get isIconRight(): boolean {
     return !!this.icon && this.iconPosition === 'right';
+  }
+
+  togglePassword() {
+    this.showPassword = !this.showPassword;
+    this.type = this.showPassword ? 'text' : 'password';
   }
 }
