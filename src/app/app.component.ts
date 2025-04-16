bf145ce4460995ddb3a8a35d60f0bb57e0ad5e49
Marcus Vinicius <m.vinicius1122o@gmail.com>
2025-04-16 12:56:45 -0300

    feat: created switch component

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,10 +2,11 @@ import { Component } from '@angular/core';
 import { ButtonComponent } from '../shared/components/button/button.component';
 import { InputComponent } from "../shared/components/input/input.component";
 import { LucideAngularModule, LockKeyhole, Mail, Phone, User, Signature } from 'lucide-angular';
+import { SwitchComponent } from '../shared/components/switch/switch.component';
 
 @Component({
   selector: 'app-root',
-  imports: [ButtonComponent, InputComponent, LucideAngularModule],
+  imports: [ButtonComponent, InputComponent, SwitchComponent, LucideAngularModule],
   templateUrl: './app.component.html',
   styleUrl: './app.component.css'
 })
