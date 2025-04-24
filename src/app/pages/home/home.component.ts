7d8d209d6d9324c3b30cfd0b6f7648db20b0db4e
JoaoCarv30 <Carvalhoestudante1@gmail.com>
2025-04-24 11:11:05 -0300

    checkbox

--- src/app/pages/home/home.component.ts
+++ src/app/pages/home/home.component.ts
@@ -1,13 +1,19 @@
-import { Component } from '@angular/core';
+import { Component, Input } from '@angular/core';
 import { ButtonComponent } from "../../../shared/components/button/button.component";
 import { Mail } from 'lucide-angular';
+import { CheckboxComponent } from "../../../shared/components/checkbox/checkbox/checkbox.component";
 
 @Component({
   selector: 'app-home',
-  imports: [ButtonComponent],
+  imports: [ButtonComponent, CheckboxComponent],
   templateUrl: './home.component.html',
   styleUrl: './home.component.css'
 })
 export class HomeComponent {
   readonly Mail = Mail;
+
+
+    cont : number = 0;
+
+
 }
